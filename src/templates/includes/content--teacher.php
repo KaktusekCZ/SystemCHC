e9a3b6cddff8cecbf8cf113a2566b6e0e1a171fc
Petr Charvát <kontakt@charvatpetr.cz>
2019-01-29 21:03:26 +0100

    main structure for student account editing

--- src/templates/includes/content--teacher.php
+++ src/templates/includes/content--teacher.php
@@ -1,7 +1,9 @@
-<div class="tab-pane fade show active" id="create-vote" role="tabpanel" aria-labelledby="create-vote-tab">
+<div class="tab-pane fade" id="create-vote" role="tabpanel" aria-labelledby="create-vote-tab">
     <p>This is for creating events. Im very good angličtin</p>
 </div>
-
-<div class="tab-pane fade" id="student-setting" role="tabpanel" aria-labelledby="student-setting-tab">
-    <p>Toto je můj píseček na kterém teď budu dělat</p>
+<!-- //TODO change active element -->
+<div class="tab-pane fade show active" id="student-setting" role="tabpanel" aria-labelledby="student-setting-tab">
+    <?php
+    require(__DIR__ . '/../includes/edit-account.php');
+    ?>
 </div>
